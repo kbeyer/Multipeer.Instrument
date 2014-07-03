a563eb75362213f5c04bf1111efd36b567a5d8c3
kbeyer <kyle.beyer@gmail.com>
2014-07-03 17:03:07 -0400

    new AEAudioReciever for streaming mic audio

---
+++ Source/MicInput/MPIAudioStreamer.h
@@ -0,0 +1,73 @@
+//
+//  MPIOutputStreamChannel.h
+//  Multipeer.Instrument
+//
+//  Inspired by AERecord
+//
+//  Created by Kyle Beyer on 7/3/14.
+//  Copyright (c) 2014 Kyle Beyer. All rights reserved.
+//
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+#import <Foundation/Foundation.h>
+#import "TheAmazingAudioEngine.h"
+    
+    extern NSString * MPIStreamerDidEncounterErrorNotification;
+    extern NSString * kMPIStreamerErrorKey;
+    
+    /*!
+     * Streaming utility, used for streaming live audio
+     *
+     *  This can be used to stream just the microphone input, or the output of the
+     *  audio system, just one channel, or a combination of all three. Simply add an
+     *  instance of this class as an audio receiver for the particular audio you wish
+     *  to record, using AEAudioController's [addInputReceiver:](@ref AEAudioController::addInputReceiver:),
+     *  [addOutputReceiver:](@ref AEAudioController::addOutputReceiver:),
+     *  [addOutputReceiver:forChannel:](@ref AEAudioController::addOutputReceiver:forChannel:), etc, and all
+     *  streams will be mixed together and recorded.
+     *
+     */
+    @interface MPIAudioStreamer : NSObject<AEAudioReceiver>
+
+
+/*!
+ * Determine whether AAC encoding is possible on this device
+ */
++ (BOOL)AACEncodingAvailable;
+
+/*!
+ * Initialise
+ *
+ * @param audioController The Audio Controller
+ */
+- (id)initWithAudioController:(AEAudioController*)audioController;
+
+/*!
+ * Prepare and begin streaming
+ *
+ *  Prepare to stream, then start streaming immediately
+ *
+ * @param stream The NSOutputStream to stream to */
+- (void)beginStreaming:(NSOutputStream*)stream;
+
+
+/*!
+ * Finish streaming and close file
+ */
+- (void)finishStreaming;
+
+
+/*!
+ * Current recorded time in seconds
+ */
+@property (nonatomic, readonly) double currentTime;
+
+@end
+    
+#ifdef __cplusplus
+}
+#endif
---
+++ Source/MicInput/MPIAudioStreamer.m
@@ -0,0 +1,158 @@
+//
+//  MPIOutputStreamChannel.m
+//  Multipeer.Instrument
+//
+//  Created by Kyle Beyer on 7/3/14.
+//  Copyright (c) 2014 Kyle Beyer. All rights reserved.
+//
+
+#import "MPIAudioStreamer.h"
+#import "AEMixerBuffer.h"
+#import "TDAudioStream.h"
+
+#define kProcessChunkSize 8192
+
+NSString * MPIStreamerDidEncounterErrorNotification = @"MPIStreamerDidEncounterErrorNotification";
+NSString * kMPIStreamerErrorKey = @"error";
+
+@interface MPIAudioStreamer ()  <TDAudioStreamDelegate> {
+    BOOL _streaming;
+    AudioBufferList *_buffer;
+    AEAudioController *_audioController;
+}
+@property (nonatomic, strong) AEMixerBuffer *mixer;
+@property (nonatomic, strong) TDAudioStream *audioStream;
+@end
+
+@implementation MPIAudioStreamer
+@synthesize mixer = _mixer, currentTime = _currentTime;
+
++ (BOOL)AACEncodingAvailable {
+    return [AEAudioFileWriter AACEncodingAvailable];
+}
+
+- (id)initWithAudioController:(AEAudioController*)audioController {
+    if ( !(self = [super init]) ) return nil;
+    _audioController = audioController;
+    self.mixer = [[AEMixerBuffer alloc] initWithClientFormat:audioController.audioDescription];
+    
+    if ( audioController.audioInputAvailable && audioController.inputAudioDescription.mChannelsPerFrame != audioController.audioDescription.mChannelsPerFrame ) {
+        [_mixer setAudioDescription:*AEAudioControllerInputAudioDescription(audioController) forSource:AEAudioSourceInput];
+    }
+    _buffer = AEAllocateAndInitAudioBufferList(audioController.audioDescription, 0);
+    
+    return self;
+}
+
+-(void)dealloc {
+    free(_buffer);
+}
+
+-(void)beginStreaming:(NSOutputStream *)stream {
+    _currentTime = 0.0;
+    
+    self.audioStream = [[TDAudioStream alloc] initWithOutputStream:stream];
+    self.audioStream.delegate = self;
+    
+    [_audioStream open];
+    
+    _streaming = YES;
+}
+
+- (void)finishStreaming {
+    _streaming = NO;
+    [self.audioStream close];
+}
+
+struct reportError_t { void *THIS; OSStatus result; };
+static void reportError(AEAudioController *audioController, void *userInfo, int length) {
+    struct reportError_t *arg = userInfo;
+    NSError *error = [NSError errorWithDomain:NSOSStatusErrorDomain
+                                         code:arg->result
+                                     userInfo:@{NSLocalizedDescriptionKey: [NSString stringWithFormat:NSLocalizedString(@"Error while streaming audio: Code %d", @""), arg->result]}];
+    [[NSNotificationCenter defaultCenter] postNotificationName:MPIStreamerDidEncounterErrorNotification
+                                                        object:(__bridge id)arg->THIS
+                                                      userInfo:@{kMPIStreamerErrorKey: error}];
+}
+
+static void audioCallback(__unsafe_unretained MPIAudioStreamer *THIS,
+                          __unsafe_unretained AEAudioController *audioController,
+                          void                     *source,
+                          const AudioTimeStamp     *time,
+                          UInt32                    frames,
+                          AudioBufferList          *audio) {
+    if ( !THIS->_streaming ) return;
+    
+    // add to mixer
+    AEMixerBufferEnqueue(THIS->_mixer, source, audio, frames, time);
+    
+    // Let the mixer buffer provide the audio buffer
+    for ( int i=0; i<THIS->_buffer->mNumberBuffers; i++ ) {
+        THIS->_buffer->mBuffers[i].mData = NULL;
+        THIS->_buffer->mBuffers[i].mDataByteSize = 0;
+    }
+    
+    THIS->_currentTime += AEConvertFramesToSeconds(audioController, frames);
+ 
+    
+    UInt32 bufferLength = kProcessChunkSize;
+    AEMixerBufferDequeue(THIS->_mixer, THIS->_buffer, &bufferLength, NULL);
+    
+    if ( bufferLength > 0 ) {
+        
+        for (NSUInteger i = 0; i < THIS->_buffer->mNumberBuffers; i++) {
+            AudioBuffer audioBuffer = THIS->_buffer->mBuffers[i];
+            [THIS->_audioStream writeData:audioBuffer.mData maxLength:audioBuffer.mDataByteSize];
+            
+            NSLog(@"buffer size: %u", (unsigned int)audioBuffer.mDataByteSize);
+        }
+    }
+}
+
+-(AEAudioControllerAudioCallback)receiverCallback {
+    return audioCallback;
+}
+
+- (void)sendDataChunk
+{
+    
+    UInt32 bufferLength = kProcessChunkSize;
+    AEMixerBufferDequeue(_mixer, _buffer, &bufferLength, NULL);
+    
+    if ( bufferLength > 0 ) {
+        
+        for (NSUInteger i = 0; i < _buffer->mNumberBuffers; i++) {
+            AudioBuffer audioBuffer = _buffer->mBuffers[i];
+            [self.audioStream writeData:audioBuffer.mData maxLength:audioBuffer.mDataByteSize];
+            
+            NSLog(@"buffer size: %u", (unsigned int)audioBuffer.mDataByteSize);
+        }
+    }
+}
+
+
+#pragma mark - TDAudioStreamDelegate
+
+- (void)audioStream:(TDAudioStream *)audioStream didRaiseEvent:(TDAudioStreamEvent)event
+{
+    switch (event) {
+        case TDAudioStreamEventWantsData:
+            [self sendDataChunk];
+            break;
+            
+        case TDAudioStreamEventError:
+            // TODO: shit!
+            NSLog(@"AudioOutputStreamer Stream Error");
+            break;
+            
+        case TDAudioStreamEventEnd:
+            // TODO: shit!
+            NSLog(@"AudioOutputStreamer Stream Ended");
+            break;
+            
+        default:
+            break;
+    }
+}
+
+@end
